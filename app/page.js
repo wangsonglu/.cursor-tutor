543379508948daf9ca6577fc265c3b80a1d9925a
wangsonglu <wangsonglu@gmail.com>
2024-11-03 17:47:26 +0800

    update race history

---
+++ app/page.js
@@ -0,0 +1,79 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div class="overflow-hidden bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      <div class="lg:pr-8 lg:pt-4">
+        <div class="lg:max-w-lg">
+          <h2 class="text-base/7 font-semibold text-indigo-600">hello world</h2>
+          <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Lulu's ultra log</p>
+          <p class="mt-6 text-lg/8 text-gray-600">A record of my running journey.</p>
+          <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+            <div class="relative pl-9">
+            <dt className="font-semibold text-gray-900">
+  🏃‍♂️ 2023 
+</dt>
+              <dd className="inline">
+  <div className="space-y-2 text-sm text-gray-600">
+    <div>🥷 02.17 香港100 56K，9:11</div>
+    <div>🥷 03.11 香港TNF 50K，9:58</div>
+    <div>🐎 03.19 深圳宝安半马，1:58</div>
+    <div>🐎 03.26 深圳盐田半马，1:58</div>
+    <div>🥷 04.15 宁海By UTMB 60K，9:53</div>
+    <div>🥷 05.14 银湖山 26K，5:20</div>
+    <div>🥷 07.08 崇礼50K，9:49</div>
+    <div>🥷 08.05 10H DBVR 10laps</div>
+    <div>🥷 09.02 武功山 50K，12:13</div>
+    <div>🐎 09.24 香港十公里锦标赛 10K，0:52</div>
+    <div>🥷 10.08 Lantau 2 peaks 23K15，2:21</div>
+    <div>🥷 10.13 Moontrekker 30K 组队 7:58</div>
+    <div>🥷 10.28 柴古 55K 8:52</div>
+    <div>🐎 11.05 迪士尼半马 2:07</div>
+    <div>🥷 11.11 香港大屿山By UTMB 50K 8:52</div>
+    <div>🐎 11.19 港珠澳大桥半马 1:48</div>
+    <div>🥷 11.26 10H DBVR 15laps</div>
+    <div>🐎 12.17 香港街马 首马 4:39</div>
+  </div>
+</dd>
+            </div>
+            <div class="relative pl-9">
+            <dt className="font-semibold text-gray-900">
+  🏃‍♂️ 2024  
+</dt>
+              <dd className="inline">
+  <div className="space-y-2 text-sm text-gray-600">
+    <div>🥷 01.20 HK100 100K | DNF</div>
+    <div>🥷 02.03 CP10H | 6 laps</div>
+    <div>🥷 03.09 ZH10H | 7 laps</div>
+    <div>🥷 03.16 LT70 | DNF</div>
+    <div>🥷 04.27 UTMF 70K | 14:56</div>
+    <div>🥷 07.13 Chongli168 100K | 22:34</div>
+    <div>🥷 08.30 UTMB CCC | 22:54</div>
+    <div>🥷 10.19 Ninghai 65K by UTMB</div>
+    <div>🥷 11.09 Translantau 50K by UTMB</div>
+    <div>🥷 12.13 TNF HK 100 relay</div>
+  </div>
+</dd>
+            </div>
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                  <path d="M4.632 3.533A2 2 0 0 1 6.577 2h6.846a2 2 0 0 1 1.945 1.533l1.976 8.234A3.489 3.489 0 0 0 16 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234Z" />
+                  <path fill-rule="evenodd" d="M4 13a2 2 0 1 0 0 4h12a2 2 0 1 0 0-4H4Zm11.24 2a.75.75 0 0 1 .75-.75H16a.75.75 0 0 1 .75.75v.01a.75.75 0 0 1-.75.75h-.01a.75.75 0 0 1-.75-.75V15Zm-2.25-.75a.75.75 0 0 0-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75h-.01Z" clip-rule="evenodd" />
+                </svg>
+               2025
+              </dt>
+              <dd class="inline">Yet to be continued...</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+  );
+}
